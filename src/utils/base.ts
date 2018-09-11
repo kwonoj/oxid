04cd33f0c7f971fcde2bbc879e2872da0290b09c
OJ Kwon <kwon.ohjoong@gmail.com>
2018-09-11 15:30:37 -0700

    feat(isstandardbrowserenv): implement isstandardbrowserenv

--- src/utils/base.ts
+++ src/utils/base.ts
@@ -49,4 +49,29 @@ const isURLSearchParams = (val: unknown) => {
   return (rootCtorDefined && val instanceof rootCtor) || (isNode() ? urlCtorDefined && val instanceof urlCtor : false);
 };
 
-export { isObject, isDate, isURLSearchParams };
+/**
+ * Determine if we're running in a standard browser environment
+ *
+ * This allows axios to run in a web worker, and react-native.
+ * Both environments support XMLHttpRequest, but not fully standard globals.
+ *
+ * web workers:
+ *  typeof window -> undefined
+ *  typeof document -> undefined
+ *
+ * react-native:
+ *  navigator.product -> 'ReactNative'
+ * nativescript
+ *  navigator.product -> 'NativeScript' or 'NS'
+ */
+const isStandardBrowserEnv = () => {
+  if (
+    typeof navigator !== 'undefined' &&
+    (navigator.product === 'ReactNative' || navigator.product === 'NativeScript' || navigator.product === 'NS')
+  ) {
+    return false;
+  }
+  return typeof window !== 'undefined' && typeof document !== 'undefined';
+};
+
+export { isObject, isDate, isURLSearchParams, isStandardBrowserEnv };
