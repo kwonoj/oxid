4fc71dc56382ca94a12802099eca86b81ad043e1
OJ Kwon <kwon.ohjoong@gmail.com>
2018-09-11 15:28:02 -0700

    feat(isabsoluteurl): implement isabsoluteurl

--- spec/utils/urls-spec.ts
+++ spec/utils/urls-spec.ts
@@ -1,5 +1,5 @@
 import { URLSearchParams } from 'url';
-import { buildURL, combineURLs } from '../../src/utils/urls';
+import { buildURL, combineURLs, isAbsoluteURL } from '../../src/utils/urls';
 
 describe('urls', () => {
   describe('buildURL', () => {
@@ -110,6 +110,28 @@ describe('urls', () => {
 
     it('should allow a single slash for relative url', () => {
       expect(combineURLs('https://api.github.com/users', '/')).toBe('https://api.github.com/users/');
+    });
+  });
+
+  describe('isAbsoluteURL', () => {
+    it('should return true if URL begins with valid scheme name', () => {
+      expect(isAbsoluteURL('https://api.github.com/users')).toBe(true);
+      expect(isAbsoluteURL('custom-scheme-v1.0://example.com/')).toBe(true);
+      expect(isAbsoluteURL('HTTP://example.com/')).toBe(true);
+    });
+
+    it('should return false if URL begins with invalid scheme name', () => {
+      expect(isAbsoluteURL('123://example.com/')).toBe(false);
+      expect(isAbsoluteURL('!valid://example.com/')).toBe(false);
+    });
+
+    it('should return true if URL is protocol-relative', () => {
+      expect(isAbsoluteURL('//example.com/')).toBe(true);
+    });
+
+    it('should return false if URL is relative', () => {
+      expect(isAbsoluteURL('/foo')).toBe(false);
+      expect(isAbsoluteURL('foo')).toBe(false);
     });
   });
 });
--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -76,4 +76,16 @@ const buildURL = (
 const combineURLs = (base: string, relative: string): string =>
   relative ? base.replace(/\/+$/, '') + '/' + relative.replace(/^\/+/, '') : base;
 
-export { buildURL, combineURLs };
+/**
+ * Determines whether the specified URL is absolute
+ *
+ * @param {string} url The URL to test
+ * @returns {boolean} True if the specified URL is absolute, otherwise false
+ *
+ * A URL is considered absolute if it begins with "<scheme>://" or "//" (protocol-relative URL).
+ * RFC 3986 defines scheme name as a sequence of characters beginning with a letter and followed
+ * by any combination of letters, digits, plus, period, or hyphen.
+ */
+const isAbsoluteURL = (url: string): boolean => /^([a-z][a-z\d\+\-\.]*:)?\/\//i.test(url);
+
+export { buildURL, combineURLs, isAbsoluteURL };
