ec8d2b4c4434d632cd928373314c5de29a657cb3
OJ Kwon <kwon.ohjoong@gmail.com>
2018-09-15 00:06:24 -0700

    test(wallaby): update test configuration

--- wallaby.js
+++ wallaby.js
@@ -1,16 +1,13 @@
 module.exports = () => ({
-  files: ['src/**/*.ts', 'jest.json', { pattern: 'dist/**/*', ignore: true }],
+  files: [
+    'package.json',
+    'src/**/*.ts',
+    { pattern: 'spec/__fixture__/**/*', instrument: false, load: true },
+    { pattern: 'dist/**/*', ignore: true }
+  ],
   tests: ['spec/**/*-spec.ts'],
-  testFramework: {
-    type: 'jest'
-  },
+
   env: {
     type: 'node'
-  },
-  setup: wallaby => {
-    const { testEnvironment } = require('./jest.json');
-    wallaby.testFramework.configure({
-      testEnvironment
-    });
   }
 });
