efe70513b0ddd7cb1c0c8bde103bd5c3a1396b62
OJ Kwon <kwon.ohjoong@gmail.com>
2018-09-17 09:52:04 -0700

    refactor(defaultoptions): freeze object

--- src/defaultOptions.ts
+++ src/defaultOptions.ts
@@ -33,69 +33,73 @@ const getDefaultAdapter = (): Adapter => (isNode() ? require('./adapters/http') 
  */
 const validateStatus = (status: number) => status >= 200 && status < 300;
 
+const defaultTransformRequest = (data: object | string, headers?: Array<Record<string, any>>) => {
+  normalizeHeaderName(headers, 'Accept');
+  normalizeHeaderName(headers, 'Content-Type');
+
+  if (isFormData(data) || isArrayBuffer(data) || isBuffer(data) || isStream(data) || isFile(data) || isBlob(data)) {
+    return data;
+  }
+  if (ArrayBuffer.isView(data)) {
+    return data.buffer;
+  }
+  if (isURLSearchParams(data)) {
+    setContentTypeIfUnset(headers, 'application/x-www-form-urlencoded;charset=utf-8');
+    return data.toString();
+  }
+  if (isObject(data)) {
+    setContentTypeIfUnset(headers, 'application/json;charset=utf-8');
+    return JSON.stringify(data);
+  }
+  return data;
+};
+
+const defaultTransformResponse = (data: any) => {
+  if (typeof data === 'string') {
+    try {
+      data = JSON.parse(data);
+    } catch (e) {
+      /* noop */
+    }
+  }
+  return data;
+};
+
 /**
  * Default configuration values.
  */
-const defaultOptions: Readonly<RequestConfig> = {
-  adapter: getDefaultAdapter(),
+const defaultOptions: Readonly<RequestConfig> = (() => {
+  let ret = {
+    adapter: getDefaultAdapter(),
 
-  transformRequest: [
-    (data: object | string, headers?: Array<Record<string, any>>) => {
-      normalizeHeaderName(headers, 'Accept');
-      normalizeHeaderName(headers, 'Content-Type');
+    transformRequest: [defaultTransformRequest],
 
-      if (isFormData(data) || isArrayBuffer(data) || isBuffer(data) || isStream(data) || isFile(data) || isBlob(data)) {
-        return data;
+    transformResponse: [defaultTransformResponse],
+
+    xsrfCookieName: 'XSRF-TOKEN',
+    xsrfHeaderName: XSRF_HEADER_NAME,
+
+    maxContentLength: -1,
+
+    validateStatus,
+
+    headers: {
+      common: {
+        Accept: 'application/json, text/plain, */*'
       }
-      if (ArrayBuffer.isView(data)) {
-        return data.buffer;
-      }
-      if (isURLSearchParams(data)) {
-        setContentTypeIfUnset(headers, 'application/x-www-form-urlencoded;charset=utf-8');
-        return data.toString();
-      }
-      if (isObject(data)) {
-        setContentTypeIfUnset(headers, 'application/json;charset=utf-8');
-        return JSON.stringify(data);
-      }
-      return data;
     }
-  ],
-
-  transformResponse: [
-    (data: any) => {
-      if (typeof data === 'string') {
-        try {
-          data = JSON.parse(data);
-        } catch (e) {
-          /* noop */
-        }
-      }
-      return data;
-    }
-  ],
-
-  xsrfCookieName: 'XSRF-TOKEN',
-  xsrfHeaderName: XSRF_HEADER_NAME,
-
-  maxContentLength: -1,
-
-  validateStatus,
-
-  headers: {
-    common: {
-      Accept: 'application/json, text/plain, */*'
-    }
-  }
-};
-
-['delete', 'get', 'head'].forEach(method => (defaultOptions.headers[method] = {}));
-['post', 'put', 'patch'].forEach(method => {
-  const current = defaultOptions.headers[method] || {};
-  defaultOptions.headers[method] = {
-    ...current,
-    ...DEFAULT_CONTENT_TYPE
   };
-});
+
+  ['delete', 'get', 'head'].forEach(method => (ret.headers[method] = {}));
+  ['post', 'put', 'patch'].forEach(method => {
+    const current = ret.headers[method] || {};
+    ret.headers[method] = {
+      ...current,
+      ...DEFAULT_CONTENT_TYPE
+    };
+  });
+
+  return Object.freeze(ret);
+})();
 
 export { defaultOptions, validateStatus };
